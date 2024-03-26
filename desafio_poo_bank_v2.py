2f50946d911aab985e2967a2895563ccce77480a
JBO21 <jackson.jbo@gmail.com>
2024-03-26 20:15:48 -0300

    novo desafio

---
+++ desafio_poo_bank_v2.py
@@ -0,0 +1,30 @@
+import textwrap
+from abc import ABC, abstractclassmethod, abstractproperty
+from datetime import datetime   
+
+class Cliente:
+    pass
+
+class PessoaFisica(Cliente):
+    pass
+
+class PessoaJuridica(Cliente):
+    pass
+
+class Conta:
+    pass
+
+class ContaCorrente(Conta):
+    pass
+
+class Historico:
+    pass
+
+class Transacao(ABC):
+    pass
+
+class Saque(Transacao):
+    pass
+
+class Deposito(Transacao):
+    pass
